52af062bd26375b5783fd2da9b6caf7d0d7dfc1a
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2023-07-25 02:40:31 +0200

    added handling of defining validation in the class

---
+++ AutoSpectre.SourceGeneration/ConfirmedValidator.cs
@@ -0,0 +1,11 @@
+namespace AutoSpectre.SourceGeneration;
+
+internal class ConfirmedValidator
+{
+    public ConfirmedValidator(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
---
+++ AutoSpectre.SourceGeneration/SinglePropertyEvaluationContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace AutoSpectre.SourceGeneration;
+
+internal class SinglePropertyEvaluationContext
+{
+    public SinglePropertyEvaluationContext(IPropertySymbol property, bool isNullable, ITypeSymbol type, bool isEnumerable, ITypeSymbol underlyingType)
+    {
+        Property = property;
+        IsNullable = isNullable;
+        Type = type;
+        IsEnumerable = isEnumerable;
+        UnderlyingType = underlyingType;
+    }
+
+    public IPropertySymbol Property { get; }
+    public bool IsNullable { get; }
+    public ITypeSymbol Type { get; }
+    public bool IsEnumerable { get; }
+    public ITypeSymbol? UnderlyingType { get; }
+    
+    public ConfirmedConverter? ConfirmedConverter { get; set; }
+    
+    public ConfirmedValidator? ConfirmedValidator { get; set; }
+}
