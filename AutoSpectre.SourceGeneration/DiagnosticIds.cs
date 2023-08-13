7f4c3edf335b2c31dd1dd4448c174ad4f4e305af
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2023-08-13 16:19:32 +0200

    Add diagnostic error when pagesize is less than 3

--- AutoSpectre.SourceGeneration/DiagnosticIds.cs
+++ AutoSpectre.SourceGeneration/DiagnosticIds.cs
@@ -8,4 +8,5 @@ public static class DiagnosticIds
     public const string Id0010_ConditionNameMatchInvalid = "AutoSpectre_JJK0010";
     public const string Id0011_ConditionNameNotFound = "AutoSpectre_JJK0011";
     public const string Id0012_UnsupportedDefaultValue = "AutoSpectre_JJK0012";
+    public const string Id0013_PageSizeMustBeThreeOrLarger = "AutoSpectre_JJK0013";
 }
