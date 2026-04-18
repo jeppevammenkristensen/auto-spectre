61ea62960004170d70ddb82a6aa52cbcc0d51b80
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-04-19 01:15:50 +0200

    Fix issue when no namespace was used for the AutoSpectreForm wrapped class

---
+++ Src/Test2/Program.cs
@@ -0,0 +1,15 @@
+﻿using AutoSpectre;
+
+
+var someForm = new SomeForm();
+someForm.Prompt();
+
+
+[AutoSpectreForm]
+public class SomeForm
+{
+    [TextPrompt(ClearOnFinish = true, Title = "Enter your name", EditableDefaultValue = true, DefaultValueSource = nameof(NameDefault))]
+    public string Name {get;set;} = string.Empty;
+    
+    public readonly string NameDefault = "Jeppe";
+}
